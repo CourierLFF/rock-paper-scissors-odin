63eb5f9d0ef46617e9acb4861502a7abdc387d3f
CourierID <45730525+CourierID@users.noreply.github.com>
2023-03-09 22:51:44 -0500

    Create playRound function and main game loop

--- main.js
+++ main.js
@@ -2,14 +2,42 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     let computerChoice;
     if (randomNumber == 0) {
-        computerChoice = 'Rock';
+        computerChoice = 'rock';
     }
     else if (randomNumber == 1) {
-        computerChoice = 'Paper';
+        computerChoice = 'paper';
     }
     else {
-        computerChoice = 'Scissors';
+        computerChoice = 'scissors';
     }
-    console.log(computerChoice);
+    return computerChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "It's a draw!";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You Lose! Paper beats Rock!";
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You Win! Rock beats Scissors! ";
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You Win! Paper beats Rock!";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You Lose! Scissors beats Paper!";
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose! Rock beats Scissors!";
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! Scissors beats paper!";
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose: ");
+        let result = playRound(playerSelection, getComputerChoice());
+        console.log(result);
+    }
+}
+
+game();
