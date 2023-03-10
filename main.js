c37ee387054277bfcae9a703f1d59188856b3b92
CourierID <45730525+CourierID@users.noreply.github.com>
2023-03-09 22:37:39 -0500

    Add getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if (randomNumber == 0) {
+        computerChoice = 'Rock';
+    }
+    else if (randomNumber == 1) {
+        computerChoice = 'Paper';
+    }
+    else {
+        computerChoice = 'Scissors';
+    }
+    console.log(computerChoice);
+}
+
